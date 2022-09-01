29264477bf66704775155d21dcd58fba17d1fbe6
Magdiel <mvg140386@gmail.com>
2022-09-01 16:08:49 -0500

    Segundo commit

---
+++ Controllers/CitasController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace taller_mecanico.Controllers
+{
+    public class CitasController
+    {
+        
+    }
+}
