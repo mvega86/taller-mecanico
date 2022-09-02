6823a32c1fa32118ad2a6ff747dc9fa1bfd27f23
Magdiel <mvg140386@gmail.com>
2022-09-02 14:52:49 -0500

    new file:   Models/Appoiment.cs

---
+++ Models/Appoiment.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace taller_mecanico.Models
+{
+    public class Appoiment
+    {
+        public int id { get; set; }
+        public DateTime datetime { get; set; } = new DateTime();
+        //public String dni { get; set; }
+    }
+}
